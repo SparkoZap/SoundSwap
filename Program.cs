3d4ce1a7e9673e795ccb520ce7e374953a72a0f2
SparkoZap <sparkozap@gmail.com>
2026-03-27 14:33:27 -0500

    feat: mute all other processes when priority is playing

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi.Interfaces;
 
 /*
     Mute all other apps when the selected app hits a certain threshold audio, to maximize focus.
@@ -12,24 +13,48 @@ public class Program
     public static void Main(string[] args)
     {
         // Capture Processes Outputting Sound.
-        Console.WriteLine("\nSelect a Main Audio Source");
-        Console.WriteLine("---------------------------");
 
-        List<Process> processes = new();
+        bool isPlaying = false;
+
+        Dictionary<int, AudioSessionControl> processes = new();
 
         var deviceEnumerator = new MMDeviceEnumerator();
-        var defaultDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+        var defaultDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
+
+        Console.WriteLine("\nSelect a Priority Audio Source");
+        Console.WriteLine("---------------------------");
 
         var audioSession = defaultDevice.AudioSessionManager;
         for (int i = 0; i < audioSession.Sessions.Count; i++)
         {
             var process = Process.GetProcessById((int)audioSession.Sessions[i].GetProcessID);
-            if (audioSession.Sessions[i].State == NAudio.CoreAudioApi.Interfaces.AudioSessionState.AudioSessionStateActive)
+            Console.WriteLine($"{i}. {process.ProcessName}");
+            processes.Add(i, audioSession.Sessions[i]);
+        }
+
+        Console.Write("\nChoose From The List [1, 2, 3...]: ");
+
+        var priority = processes[Convert.ToInt32(Console.ReadLine())];
+        Console.WriteLine($"You've selected {priority.DisplayName}");
+
+        while(true) 
+        {
+            if (priority.State.Equals(AudioSessionState.AudioSessionStateActive))
             {
-                Console.WriteLine($"{i}. {process.ProcessName} is currently playing audio");
+                isPlaying = true;
+            } 
+            else
+            {
+                isPlaying = false;
+            }
+
+            foreach (var proc in processes.Values)
+            {
+                if (proc != priority)
+                {
+                    proc.SimpleAudioVolume.Volume = isPlaying ? 0 : 1;
+                }
             }
         }
-        Console.Write("\nChoose From The List [1, 2, 3...]: ");
-        var answer = Console.ReadLine();
     }
 }
